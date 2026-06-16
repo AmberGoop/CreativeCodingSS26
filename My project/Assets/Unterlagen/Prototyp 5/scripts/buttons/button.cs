81d8166257967222af2b88d1292b283750e07bb1
MintiestKitKat <AmberGoop@gmail.com>
2026-06-16 15:36:13 +0200

    quit button functionality debug

--- My project/Assets/Unterlagen/Prototyp 5/scripts/buttons/button.cs
+++ My project/Assets/Unterlagen/Prototyp 5/scripts/buttons/button.cs
@@ -1,16 +1,13 @@
+using System;
 using UnityEngine;
 
 public class button : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+    public void quitGame()
     {
-        
+        Debug.Log("quit game");
     }
 }
